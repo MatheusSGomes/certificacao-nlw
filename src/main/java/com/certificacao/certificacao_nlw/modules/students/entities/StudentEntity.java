ff33a3fa9bb78148b951a975078236de30a121aa
Matheus Gomes <math.s.gomes@gmail.com>
2024-02-12 17:35:48 -0300

    adicionadas entities student answers e certification

---
+++ src/main/java/com/certificacao/certificacao_nlw/modules/students/entities/StudentEntity.java
@@ -0,0 +1,19 @@
+package com.certificacao.certificacao_nlw.modules.students.entities;
+
+import java.util.UUID;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudentEntity {
+
+    private UUID id;
+    private String email;
+    private List<CertificationStudentEntity> certificationStudentEntity;
+
+}
