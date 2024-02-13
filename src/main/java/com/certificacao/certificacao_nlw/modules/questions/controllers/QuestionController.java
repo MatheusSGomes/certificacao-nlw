d9f6a93e1ce66ed59a74463687fd267e9caea367
Matheus Gomes <math.s.gomes@gmail.com>
2024-02-13 12:48:03 -0300

    filtrar retorno questions e alternatives

--- src/main/java/com/certificacao/certificacao_nlw/modules/questions/controllers/QuestionController.java
+++ src/main/java/com/certificacao/certificacao_nlw/modules/questions/controllers/QuestionController.java
@@ -1,6 +1,7 @@
 package com.certificacao.certificacao_nlw.modules.questions.controllers;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.certificacao.certificacao_nlw.modules.questions.dto.AlternativeResultDTO;
+import com.certificacao.certificacao_nlw.modules.questions.dto.QuestionResultDTO;
+import com.certificacao.certificacao_nlw.modules.questions.entities.AlternativesEntity;
 import com.certificacao.certificacao_nlw.modules.questions.entities.QuestionEntity;
 import com.certificacao.certificacao_nlw.modules.questions.repositories.QuestionRepository;
 
@@ -19,8 +23,35 @@ public class QuestionController {
     private QuestionRepository questionRepository;
 
     @GetMapping("/technology/{technology}")
-    public List<QuestionEntity> findByTechnology(@PathVariable String technology) {
-        return this.questionRepository.findByTechnology(technology);
+    public List<QuestionResultDTO> findByTechnology(@PathVariable String technology) {
+        var result = this.questionRepository.findByTechnology(technology);
+
+        var toMap = result.stream().map(question -> mapQuestionToDto(question))
+            .collect(Collectors.toList());
+
+        return toMap;
+    }
+
+    static QuestionResultDTO mapQuestionToDto(QuestionEntity question) {
+        var questionResultDTO = QuestionResultDTO.builder()
+            .id(question.getId())
+            .technology(question.getTechnology())
+            .description(question.getDescription()).build();
+
+        List<AlternativeResultDTO> alternativeResultDTOs =
+            question.getAlternatives().stream()
+            .map(alternative -> mapAlternativeResultDTO(alternative))
+            .collect(Collectors.toList());
+
+        questionResultDTO.setAlternatives(alternativeResultDTOs);
+
+        return questionResultDTO;
+    }
+
+    static AlternativeResultDTO mapAlternativeResultDTO(AlternativesEntity alternativesResultDTO) {
+        return AlternativeResultDTO.builder()
+            .id(alternativesResultDTO.getId())
+            .description(alternativesResultDTO.getDescription()).build();
     }
 
 }
