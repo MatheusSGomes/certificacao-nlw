fd5c8d3c5772790a3db4b2eb501c9893a53da6ca
Matheus Gomes <math.s.gomes@gmail.com>
2024-02-13 20:32:27 -0300

    nota estudante

--- src/main/java/com/certificacao/certificacao_nlw/modules/students/useCases/StudentCertificationAnswersUseCase.java
+++ src/main/java/com/certificacao/certificacao_nlw/modules/students/useCases/StudentCertificationAnswersUseCase.java
@@ -3,6 +3,7 @@ package com.certificacao.certificacao_nlw.modules.students.useCases;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -32,6 +33,8 @@ public class StudentCertificationAnswersUseCase {
         List<QuestionEntity> questionEntity = questionRepository.findByTechnology(dto.getTechnology());
         List<AnswersCertificationsEntity> answersCertifications = new ArrayList<>();
 
+        AtomicInteger correctAnswers = new AtomicInteger(0);
+
         dto.getQuestionAnswers().stream().forEach(questionAnswer -> {
             var question = questionEntity.stream().filter(q -> q.getId().equals(questionAnswer.getQuestionID())).findFirst().get();
 
@@ -39,6 +42,7 @@ public class StudentCertificationAnswersUseCase {
 
             if (findCorrectAlternative.getId().equals(questionAnswer.getAlternativeID())) {
                 questionAnswer.setCorrect(true);
+                correctAnswers.incrementAndGet();
             } else {
                 questionAnswer.setCorrect(false);
             }
@@ -64,6 +68,7 @@ public class StudentCertificationAnswersUseCase {
         CertificationStudentEntity certificationStudentEntity = CertificationStudentEntity.builder()
             .technology(dto.getTechnology())
             // .answersCertificationsEntities(answersCertifications)
+            .grate(correctAnswers.get())
             .studentID(studentID)
             .build();
 
