0bf8687ae6d67be37f739c6e1c04023328ebf2b6
Matheus Gomes <math.s.gomes@gmail.com>
2024-02-12 23:27:40 -0300

    mapeamento entidades

--- src/main/java/com/certificacao/certificacao_nlw/modules/students/entities/StudentEntity.java
+++ src/main/java/com/certificacao/certificacao_nlw/modules/students/entities/StudentEntity.java
@@ -1,7 +1,17 @@
 package com.certificacao.certificacao_nlw.modules.students.entities;
 
-import java.util.UUID;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,10 +20,19 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "students")
 public class StudentEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Column(unique = true, nullable = false)
     private String email;
+
+    @OneToMany(mappedBy = "studentEntity")
     private List<CertificationStudentEntity> certificationStudentEntity;
 
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
